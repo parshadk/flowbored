b83e95599d89084c847f3b774b4fafa14fb8a16c
parshadk <parshadk04@gmail.com>
2025-06-23 23:26:31 +0530

    updates

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+    
+})
+
+app.post("/signup",(req,res)=>{
+    
+})
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3000");  
+});
